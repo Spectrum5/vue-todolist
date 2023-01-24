d3813e9e767c1f3a73dc6f0818e254f61e925136
Spectrum5 <toskusergio@gmail.com>
2023-01-24 16:49:32 +0100

    Aggiunto form con push

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,23 @@ const {createApp} = Vue;
 
 createApp({
     data(){
+        
         return{
+            item: "",
+            listItem : [ ],
 
-        }
-    }
+        };
+    },
+
+    methods:{
+        createItem(){
+            let userList = {
+                text: this.item,
+                done: false,
+            }
+            this.listItem.push(userList);                   
+            this.item = '';
+        },
+    },
+
 }).mount("#app");
